b2210fd4bdc3f2fff0b1e6080ff2423353d9a3b7
Luan Kurmann <luan@kurmann.dev>
2022-05-12 08:54:29 +0200

    improvments

--- src/Grafikeditor1/EditorControl.java
+++ src/Grafikeditor1/EditorControl.java
@@ -23,10 +23,15 @@ final class EditorControl {
 
   public void erzeugeFigurMitZweitemPunkt(Point zweiterPunkt) {
     Figur figur = null;
+    int liniendicke = 1;
+    int x = 0;
+    int y = 0;
+
     int breite = (int)(zweiterPunkt.getX() - ersterPunkt.getX());
     int laenge = (int)(zweiterPunkt.getY()- ersterPunkt.getY());
-    int x = (int) ersterPunkt.getX();
-    int y = (int) ersterPunkt.getY();
+
+    x = (int) ersterPunkt.getX();
+    y = (int) ersterPunkt.getY();
     if (breite <= 0){
       breite = breite * -1 ;
       x = (int) zweiterPunkt.getX();
@@ -35,30 +40,16 @@ final class EditorControl {
       laenge = laenge * -1 ;
       y = (int) zweiterPunkt.getY();
     }
-    int liniendicke = 1;
-    switch (figurTyp) {
-      case 'r':
-        figur = new Rechteck(x, y, laenge, breite, liniendicke);
-        break;
-      case 'l':
-        figur = new Linie((int)ersterPunkt.getX(),(int)ersterPunkt.getY(), (int)zweiterPunkt.getX(), (int)zweiterPunkt.getY(), liniendicke);
-        break;
-      case 'k':
-        figur = new Kreis(x, y, breite, liniendicke);
-        break;
-      case 's':
-        figur = new Schneemann(x, y, breite / 2, liniendicke);
-        break;
-      case 'm':
-        figur = new Schild(x, y, breite, laenge, liniendicke);
-        break;
-      case 'f':
-        figur = new SchneemannFamillie(x, y, liniendicke, breite);
-        break;
-      default:
-        figur = new Rechteck(x, y, laenge, breite, liniendicke);
-        break;
-    }
+
+    figur = switch (figurTyp) {
+      case 'l' -> new Linie((int) ersterPunkt.getX(), (int) ersterPunkt.getY(), (int) zweiterPunkt.getX(), (int) zweiterPunkt.getY(), liniendicke);
+      case 'k' -> new Kreis(x, y, breite, liniendicke);
+      case 'e' -> new Ellipse(x, y, breite, laenge, liniendicke);
+      case 's' -> new Schneemann(x, y, breite / 2, liniendicke);
+      case 'm' -> new Schild(x, y, breite, laenge, liniendicke);
+      case 'f' -> new SchneemannFamillie(x, y, liniendicke, breite);
+      default -> new Rechteck(x, y, laenge, breite, liniendicke);
+    };
     display.hinzufuegen(figur);
   }
 }
